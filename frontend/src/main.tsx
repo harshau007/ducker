d3d6feb7276a6f68141de79ebebd922ae7266b09
0amirk <amir.kamal09@gmail.com>
2024-02-17 13:15:17 +0530

    Added right side components in the refactored code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import NoImage from './components/NoImage.tsx'
+import WithImage from './components/WithImage.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <NoImage/>
+    <WithImage/>
   </React.StrictMode>,
 )
